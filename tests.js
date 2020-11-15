95dc18b3444ccbd45612b6a2ab455c362fba2254
Richard Hansen <rhansen@rhansen.org>
2020-11-15 16:02:15 -0500

    mocha/no-hooks-for-single-case: Disable rule
    Separate setup can make the test code easier to read, and it makes it
    easier to add additional cases in the future.

--- tests.js
+++ tests.js
@@ -12,6 +12,7 @@ module.exports = {
     'mocha',
   ],
   rules: {
+    'mocha/no-hooks-for-single-case': 'off',
     'mocha/no-return-from-async': 'error',
     'mocha/no-synchronous-tests': 'error',
     'mocha/prefer-arrow-callback': 'error',

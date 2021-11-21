77af18b40687985e932ad45d9307ed38c4c63396
Richard Hansen <rhansen@rhansen.org>
2021-11-21 02:04:28 -0500

    mocha/max-top-level-suites, mocha/no-global-tests: Disable for frontend tests

--- tests/frontend.js
+++ tests/frontend.js
@@ -10,4 +10,10 @@ module.exports = {
     expect: 'readonly',
     helper: 'readonly',
   },
+  rules: {
+    // Each frontend test spec file is wrapped inside a `describe()`.
+    'mocha/max-top-level-suites': 'off',
+    // Each frontend test spec file is wrapped inside a `describe()`.
+    'mocha/no-global-tests': 'off',
+  },
 };

ether/eslint-config-etherpad

commit daa9b4e08ebfa5674e107023c50e7c1d38e764b7

import/*: Enable plugin and recommended rules

index.js
@@ -7,11 +7,13 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:eslint-comments/recommended',
+    'plugin:import/recommended',
     'plugin:promise/recommended',
     'plugin:you-dont-need-lodash-underscore/compatible',
   ],
   plugins: [
     'eslint-comments',
+    'import',
     'prefer-arrow',
     'promise',
     'you-dont-need-lodash-underscore',

commit 6a4d53b7ac3c5a0071484b213a90f2e4af81baee

Explicitly default `sourceType` to `script`
This fixes a bug introduced in commit
daa9b4e08ebfa5674e107023c50e7c1d38e764b7.

index.js
@@ -53,6 +53,12 @@ module.exports = {
     'promise',
     'you-dont-need-lodash-underscore',
   ],
+  parserOptions: {
+    // plugin:import/recommended sets sourceType to module. Change it back to script because most
+    // Etherpad core and plugin code is still CommonJS. The node plugin will change it to module if
+    // package.json sets type to module.
+    sourceType: 'script',
+  },
   rules: {
     ...sharedEsTsRules,
     'array-bracket-newline': ['error', 'consistent'],